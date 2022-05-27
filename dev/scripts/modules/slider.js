d649fe1b98147f6af9dca1df4f3c8dce992d9b9f
sabbumhz <mhjsab@gmail.com>
2022-05-27 19:12:04 +0545

    updated added

--- dev/scripts/modules/slider.js
+++ dev/scripts/modules/slider.js
@@ -4,6 +4,6 @@ new fullpage('#fullpage', {
     navigation: true,
     slidesNavigation: true,
     controlArrows: false,
-    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    anchors: ['Intro', 'Hello!', 'About-Me', 'Academic-Background', 'Work-Experience','Recent-Works'],
     navigationTooltips:['Intro', 'Hello!', 'About Me','Academic Background', 'Work Experience','Recent Works'],
 });
