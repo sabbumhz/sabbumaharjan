025772a42a36124c1ef15b37d0ff18d5eb7926ba
sabbumhz <mhjsab@gmail.com>
2022-05-27 20:54:57 +0545

    updates

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,66 @@
-(()=>{"use strict";new class{}})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./dev/scripts/modules/app.js":
+/*!************************************!*\
+  !*** ./dev/scripts/modules/app.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// import 'popper.js';\r\n// import 'bootstrap/js/dist/button';\r\n// import 'bootstrap/js/dist/modal';\r\n// import 'bootstrap/js/dist/dropdown';\r\n// import 'bootstrap/js/dist/modal';\r\n// import 'bootstrap/js/dist/tab';\r\n// import 'bootstrap/js/dist/collapse';\r\n// import 'bootstrap-select';\r\n// import 'bootstrap/js/dist/modal';\r\n\r\n\r\nclass global {\r\n    // constructor() {\r\n    //     this.myModal = document.getElementById('exampleModal')\r\n    //     this.stickyHeader();\r\n    //     this.megamenuPopup();\r\n    //     this.toggleContent();\r\n    //     this.readMore();\r\n    // }\r\n\r\n    // stickyHeader() {\r\n    //     var elemBody = $('body');\r\n    //     var pageHeader = $('.theme-header');\r\n    //     if ($(elemBody).hasClass('home')) {\r\n    //         $(pageHeader).addClass('theme-header--dark');\r\n    //     }\r\n\r\n    //     $(window).scroll(function () {\r\n    //         if ($(this).scrollTop() > 50) {\r\n    //             $('.theme-header').addClass('sticky');\r\n    //         } else {\r\n    //             $('.theme-header').removeClass('sticky');\r\n    //         }\r\n    //     });\r\n    // }\r\n\r\n    // readMore() {\r\n    //     $('.readmore').click(function () {\r\n    //         $('.collapse-text').addClass('expand');\r\n    //         $(this).hide();\r\n    //     });\r\n    // }\r\n\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (global);\r\nnew global();\r\n\n\n//# sourceURL=webpack://webpack-starter-template/./dev/scripts/modules/app.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./dev/scripts/modules/app.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
