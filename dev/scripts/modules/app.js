8dd01a754380e18c983f3154479104e6478fcc6a
sabbumhz <mhjsab@gmail.com>
2022-06-10 02:57:41 +0545

    updates

--- dev/scripts/modules/app.js
+++ dev/scripts/modules/app.js
@@ -4,9 +4,9 @@
 // import 'bootstrap/js/dist/dropdown';
 // import 'bootstrap/js/dist/modal';
 // import 'bootstrap/js/dist/tab';
-// import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/offcanvas';
+import 'bootstrap/js/dist/collapse';
 // import 'bootstrap-select';
-// import 'bootstrap/js/dist/modal';
 
 
 class global {
--- dev/scripts/modules/slider.js
+++ dev/scripts/modules/slider.js
@@ -1,16 +1,3 @@
 AOS.init({
     once: true,
 });
-
-new fullpage('#fullpage', {
-    licenseKey:"null",
-    navigation: true,
-    slidesNavigation: true,
-    controlArrows: false,
-    anchors: ['Intro', 'Hello!', 'About-Me', 'Academic-Background', 'Work-Experience','Recent-Works','Academic-Projects'],
-    navigationTooltips:['Intro', 'Hello!', 'About Me','Academic Background', 'Work Experience','Recent Works', 'Academic Projects'],
-
-    afterLoad: function(){
-        $('.fp-table.active .aos-init').addClass('aos-animate');
-    },
-});
